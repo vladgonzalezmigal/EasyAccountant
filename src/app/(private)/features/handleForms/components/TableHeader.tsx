214da858a05a80b1cb611d2012a2b063c9e5b474
vladgonzalezmigal <77864847+vladgonzalezmigal@users.noreply.github.com>
2025-04-30 20:50:31 -0400

    table fully styled

--- src/app/(private)/features/handleForms/components/TableHeader.tsx
+++ src/app/(private)/features/handleForms/components/TableHeader.tsx
@@ -6,7 +6,7 @@ interface TableHeaderProps {
 
 const TableHeader: React.FC<TableHeaderProps> = ({ headerTitles }) => {
   return (
-    <div className="px-4 bg-[#F5F5F5] border border-[#DCDCDC] h-[60px] rounded-top header-shadow flex items-center relative z-10">
+    <div className="px-4 bg-[#F5F5F5] border border-b-0 border-t-2 border-x-2 border-[#ECECEE] h-[60px] rounded-top header-shadow flex items-center relative z-10">
       <div className="flex flex-row justify-between bg-[#F5F5F5] w-full px-10">
         {headerTitles.map((title, index) => (
           <div key={index} className="w-[100px] pl-4">
