3b85f6042ecc4af4425e8e3ac0d3ac5251293e40
vladgonzalezmigal <77864847+vladgonzalezmigal@users.noreply.github.com>
2025-05-09 12:23:04 -0400

    added 2nd page

--- src/app/(private)/selection/(forms)/payroll/[year]/[month]/2/page.tsx
+++ src/app/(private)/selection/(forms)/payroll/[year]/[month]/2/page.tsx
@@ -3,22 +3,40 @@
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { Loading } from "@/app/components/Loading";
-import { getDaysInMonth, months } from "@/app/(private)/utils/dateUtils";
 import TableTitle from "@/app/(private)/features/handleForms/components/TableTitle";
+import {  months } from "@/app/(private)/utils/dateUtils";
+import { getRequest } from "@/app/(private)/features/handleForms/utils/actions/crudOps";
+import { Payroll } from "@/app/(private)/types/formTypes";
+import PayrollTable from "@/app/(private)/features/handleForms/components/payrollTable/PayrollTable";
+import { getDaysInMonth } from "@/app/(private)/utils/dateUtils";
 
-export default function PayrollDocumentPagePeriod1() {
+export default function PayrollDocumentPagePeriod2() {
     const { year, month } = useParams();
+    // Set end date to be the 15th of the month
+    const lastDayOfMonth = getDaysInMonth(parseInt(month as string) - 1, parseInt(year as string));
+    const startDate = `${year}-${(month as string).padStart(2, '0')}-15`;
+    const endDate = `${year}-${(month as string).padStart(2, '0')}-${lastDayOfMonth}`;
+    console.log("end date", lastDayOfMonth);
+    // fetch logic 
     const [fetchLoading, setFetchLoading] = useState(true);
-
+    const [fetchError, setFetchError] = useState<string | null>(null);
+    const [payrollData, setPayrollData] = useState<Payroll[]>([]);
     useEffect(() => {
-        // Simulate loading for now
-        const timer = setTimeout(() => {
+        const fetchExpenses = async () => {
+            const dataType = { id: -1, end_date: '', employee_name: '', wage_type: 'salary', wage_rate: -1, hours: 0,   minutes: 0, total_pay: 0 } as Payroll;
+            const readRes = await getRequest({ tableName: 'payroll', dataType: dataType, startDate, endDate });
+            if (typeof readRes !== 'string' && !readRes.data) {
+                setFetchError(readRes.error);
+                return;
+            } else if (typeof readRes !== 'string' && readRes.data) {
+                setPayrollData(readRes.data as Payroll[]);
+            }
             setFetchLoading(false);
-        }, 1000);
-
-        return () => clearTimeout(timer);
-    }, []);
-
+        }
+        fetchExpenses();
+        
+    }, [startDate, endDate]);
+    console.log(payrollData);
     return (
         <div className="w-full h-full flex flex-col items-center justify-center">
             {fetchLoading ? (
@@ -26,22 +44,29 @@ export default function PayrollDocumentPagePeriod1() {
                     <Loading />
                 </div>
             ) : (
-                <>
+                <div className="w-full h-full flex flex-col items-center justify-center gap-y-1">
                     {/* Title */}
                     <div className="w-full text-center w-full flex flex-col items-center">
                         <TableTitle 
-                            title={`Period Ending ${months[parseInt(month as string) - 1]} ${getDaysInMonth(parseInt(month as string) - 1, parseInt(year as string))}`} 
+                            title={`Period Ending ${months[parseInt(month as string) - 1]} ${lastDayOfMonth}`} 
                             month={month as string} 
                             year={year as string} 
                             type="payroll" 
                         />
+                        <div className="flex flex-col items-center justify-center">
+                            <p className="font-semibold text-[#585858]">  </p>
+                            {/* <CudError cudError={cudError} /> */}
+                        </div>
+                    </div>
+                    <div>
+                        {fetchError && <div className="text-red-500">{fetchError}</div>}
                     </div>
 
-                    {/* Table Component - Placeholder */}
-                    <div className="w-full h-full">
-                        {/* Table will go here */}
+                    {/* Table Component */}
+                    <div className="w-full">
+                        <PayrollTable data={payrollData} save={false} onSave={() => {}} onEdit={() => {}} />
                     </div>
-                </>
+                </div>
             )}
         </div>
     );
